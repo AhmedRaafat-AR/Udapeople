c5055612efac32a9eb2e39f4c4422e35116db5cd
Ahmed Raafat <ar.ud.fw@gmail.com>
2021-05-09 14:56:43 +0200

    update files and reconfigure-infrastructure

--- .circleci/files/test.sh
+++
@@ -1,6 +0,0 @@
-aws cloudformation deploy \
-  --template-file frontend.yml \
-  --stack-name "udapeople-frontend-c09911cf-efd6-4c29-9489-2df7c83628cc" \
-  --tags project=udapeople \
-  --parameter-overrides ID="c09911cf-efd6-4c29-9489-2df7c83628cc" 
-
